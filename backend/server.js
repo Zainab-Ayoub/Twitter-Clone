03a3752d9eced0ad6be29cbc8d19b403bf7a546c
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-09 22:39:35 +0500

    fn that runs b/w req, res (middleware)

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,6 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log( `Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
     connectMongoDB();    
 });
