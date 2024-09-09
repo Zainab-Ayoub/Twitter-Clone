c61251a9cda8fd7a88e12abcbd6221320b96ccf2
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-09 22:40:49 +0500

    typo

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); // to parse req.body
 
 app.use("/api/auth", authRoutes);
 
