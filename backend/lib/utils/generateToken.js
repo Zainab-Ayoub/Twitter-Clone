dbfd0c6f7e58935de3b31cc873344866468f32dd
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-09 23:30:12 +0500

    token & cookie

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -2,6 +2,13 @@ import jwt from 'jsonwebtoken';
 
 export const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-      expiresIn: '30d'  
-    })
+      expiresIn: '15d',  
+    });
+
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000,  // ms
+        httpOnly: true,  // prevents cross-site scripting attacks
+        sameSite: "strict",  // CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== development,
+    });
 }
