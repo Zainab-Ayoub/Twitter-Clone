7ce5a89abf1f2f47f85889bc9dfef5e4720a8601
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-10 22:27:18 +0500

    cookieParser

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true})); // to parse from data (urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
