a3b1c5a46633e9ad43c0448ac3f889b6460a53f4
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-10 23:27:49 +0500

    getMe request

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -85,7 +85,7 @@ export const login = async (req, res) => {
 		console.log("Error in login controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });	
 	}
-}
+};
 
 export const logout = async (req, res) => {
     try {
@@ -95,4 +95,14 @@ export const logout = async (req, res) => {
 	  console.log("Error in logout controller", error.message);
 	  res.status(500).json({ error: "Internal Server Error"});	 
 	}
-}
+};
+
+export const getMe = async (req, res) =>{
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+	res.status(200).json(user);	
+  } catch (error) {
+	console.log("Error in getMe controller", error.message);
+	res.status(500).json({ error: "Internal Server Error" });
+  }	
+};
