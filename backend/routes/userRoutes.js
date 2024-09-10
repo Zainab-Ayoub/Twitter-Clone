7d10c0ea3593c524cfb82ec414f05e38d2243198
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-11 00:32:11 +0500

    import getUserProfile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute';
+import { getUserProfile } from '../controllers/userController';
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
-router.get("/suggested", protectRoute, getUserProfile);
-router.get("/follow/:id", protectRoute, followUnfollowUser);
-router.get("/update", protectRoute, updateUserProfile);
+// router.get("/suggested", protectRoute, getUserProfile);
+// router.get("/follow/:id", protectRoute, followUnfollowUser);
+// router.get("/update", protectRoute, updateUserProfile);
 
 export default router;
