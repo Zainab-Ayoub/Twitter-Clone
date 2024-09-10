196c3a1461be89cdf7431d5790d19d2d0b0f9427
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-10 23:48:40 +0500

    create userRoutes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { protectRoute } from '../middleware/protectRoute';
+
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getUserProfile);
+router.get("/follow/:id", protectRoute, followUnfollowUser);
+router.get("/update", protectRoute, updateUserProfile);
+
+export default router;
