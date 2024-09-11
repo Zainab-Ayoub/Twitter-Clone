205d040802543b2a17911ecea6ee9b1896cc2d5b
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-11 18:56:24 +0500

    notificationSchema

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
 import { getUserProfile } from '../controllers/userController.js';
+import { followUnfollowUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 // router.get("/suggested", protectRoute, getUserProfile);
-// router.get("/follow/:id", protectRoute, followUnfollowUser);
+router.get("/follow/:id", protectRoute, followUnfollowUser);
 // router.get("/update", protectRoute, updateUserProfile);
 
 export default router;
