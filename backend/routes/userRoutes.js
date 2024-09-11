2e7adab44652153b4d7840afa784d9b65d7f3468
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-11 22:04:36 +0500

    post methods

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.get("/suggested", protectRoute, getSuggestedUsers);
-router.get("/follow/:id", protectRoute, followUnfollowUser);
-// router.get("/update", protectRoute, updateUserProfile);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+// router.post("/update", protectRoute, updateUserProfile);
 
 export default router;
