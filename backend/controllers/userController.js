1966a4a44e02fcd352460a977f526c54e4c49fac
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-11 23:01:44 +0500

    catch error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -148,7 +148,8 @@ export const updateUser = async (req, res) => {
     
     return res.status(200).json(user);
   } catch (error) {
-    
+    console.log("Error in updateUser: ", error.message);
+    res.status(500).json({ error: error.message });
   }
 }
 
