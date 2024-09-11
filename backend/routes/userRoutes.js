b0a21be264a2520994d56311e616acd5db4c32e9
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-11 22:11:35 +0500

    name changed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
 import { getUserProfile } from '../controllers/userController.js';
 import { followUnfollowUser } from '../controllers/userController.js';
+import { getSuggestedUsers } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-// router.post("/update", protectRoute, updateUserProfile);
+router.post("/update", protectRoute, updateUser);
 
 export default router;
