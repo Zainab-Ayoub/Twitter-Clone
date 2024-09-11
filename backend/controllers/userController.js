66229a2041486799dd5a5559cce033242f54fbed
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-11 23:02:24 +0500

    const should be let

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -95,7 +95,7 @@ export const updateUser = async (req, res) => {
   const userId = req.user._id;
 
   try {
-    const user = await User.findById(userId);
+    let user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
