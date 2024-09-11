fdb1d72d3706e9bf7ecbb4e7842b8ebf8856de97
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-11 23:18:40 +0500

    mongoose.model

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -1,5 +1,37 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text:{
+    type: String,
+  },
+  img:{
+    type: String
+  },
+  likes:[
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    }
+  ],
+  likes:[
+    {
+      text: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+    },
+  ],
+ }, {timestamps: true}
+);
 
-}, {timestamps: true});
+const Post = mongoose.model("Post", postSchema);
