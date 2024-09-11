3e150bea269fbbb0e056c57d33fa0a9dc5f4c23d
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-11 22:54:07 +0500

    updateUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,3 +88,54 @@ export const getSuggestedUsers = async (req, res) => {
   }
 };
 
+export const updateUser = async (req, res) => {
+  const {fullName, email, username, currentPassword, newPassword, bio, link} = req.body;
+  let {profileImg, coverImg} = req.body;
+
+  const userId = req.user._id;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (!newPassword && currentPassword || newPassword && !currentPassword) {
+      return res.status(400).json({ error: "Please provide both current password and new password" });
+    }
+    if (currentPassword && newPassword) {
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ error: "Current password is incorrect" });
+      }
+      if (newPassword.length < 6) {
+        return res.status(400).json({ error: "Password must be 6 characters long" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+    }
+
+    if (profileImg) {
+      const uploadedResponse = await cloudinary.uploader.upload(coverImg);
+      coverImg = uploadedResponse.secure_url;
+    }
+
+    user.fullName = fullName || user.fullName;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.bio = bio || user.bio;
+    user.link = link || user.link;
+    user.profileImg = profileImg || user.profileImg;
+    user.coverImg = coverImg || user.coverImg;
+
+    user = await user.save();
+
+    //password should be null in response
+    user.password = null;
+    
+    return res.status(200).json(user);
+  } catch (error) {
+    
+  }
+}
+
