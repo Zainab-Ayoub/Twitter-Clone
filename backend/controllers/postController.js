945830eba738f911d7c8a0eb05bbcb35ff8193b0
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-12 00:35:54 +0500

    import createPost

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,3 +1,32 @@
-import Post from "../models/postModel";
-import User from "../models/userModel";
+import Post from "../models/postModel.js";
+import User from "../models/userModel.js";
 import { v2 as cloudinary } from "cloudinary";
+
+export const createPost = async (req, res) => {
+    try {
+      const {text} = req.body;
+      let {img} = req.body;
+      const userId = req.user._id.toString();
+      
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      if (!text && !img) {
+        return res.status(400).json({ error: "Post must have text or image" });
+      }
+      if (!img) {
+        const uploadedResponse = await cloudinary.uploader.upload(img);
+        img = uploadedResponse.secure_url;
+      }
+      
+      const newPost = new Post ({
+        uer: userId,
+        text,
+        img, 
+      });
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+      console.log("Error in createPost controller: ", error);    
+    }
+};
