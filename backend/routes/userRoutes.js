edccf42d63fdba85fb89d06bee4458abaffcdfb0
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-11 19:06:59 +0500

    getSuggestedUsers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,7 @@ import { followUnfollowUser } from '../controllers/userController.js';
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
-// router.get("/suggested", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.get("/follow/:id", protectRoute, followUnfollowUser);
 // router.get("/update", protectRoute, updateUserProfile);
 
