9d027a0a5d7f5dd99ddd60632cb0c7efbdb58f7e
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-12 18:47:50 +0500

    import & create notificationRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+
 import authRoutes from "./routes/authRoute.js";
 import userRoutes from "./routes/userRoute.js";
 import postRoutes from "./routes/postRoute.js";
+import notificationRoutes from "./routes/notificationRoute.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
-import cookieParser from "cookie-parser";
-import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
@@ -26,6 +29,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
