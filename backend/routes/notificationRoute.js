9912672da00c310de0eb091d0b80596174122ca6
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-12 19:02:41 +0500

    imports

--- backend/routes/notificationRoute.js
+++ backend/routes/notificationRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
+import { getNotifications, deleteNotifications } from "../controllers/notificationController.js";
 
 const router = express.Router();
 
