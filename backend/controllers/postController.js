1fecf451b6e3ad22e178590adaeadc57e1f186cc
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-12 17:00:20 +0500

    different method because populate is used

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -127,7 +127,10 @@ export const likeUnlikePost = async (req, res) => {
 
 export const getAllPosts = async (req, res) => {
   try {
-    const posts = await Post.find().sort({ createdAt: -1 }).populate("user").select("-password");
+    const posts = await Post.find().sort({ createdAt: -1 }).populate({
+      path: "user",
+      select: "-password",
+    });
 
     if (posts.length === 0) {
       return res.status(200).json([]);
