6c52c4fd5d6dbe9348839d34f4e61a2eb730eb26
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-12 19:02:05 +0500

    deleteNotifications function

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -18,3 +18,16 @@ export const getNotifications = async (req, res) => {
       res.status(500).json({ error: "Internal Server Error" });  
     }
 };
+
+export const deleteNotifications = async (req, res) => {
+    try {
+      const userId = req.user._id;
+      
+      await Notification.deleteMany({ to: userId });
+
+      res.status(200).json({ message: "Notifications deleted successfully" });
+    } catch (error) {
+      console.log("Error in deleteNotifications function: ", error.message);
+      res.status(500).json({ error: "Internal Server Error" });  
+    }
+};
