cc4eab432372e1800e26b661ff71d770bd2abd82
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-12 19:12:38 +0500

    deleteOneNotification

--- backend/routes/notificationRoute.js
+++ backend/routes/notificationRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { getNotifications, deleteNotifications } from "../controllers/notificationController.js";
+import { getNotifications, deleteNotifications, deleteOneNotification } from "../controllers/notificationController.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
 router.delete("/", protectRoute, deleteNotifications);
+router.delete("/:id", protectRoute, deleteOneNotification);
 
 export default router;
