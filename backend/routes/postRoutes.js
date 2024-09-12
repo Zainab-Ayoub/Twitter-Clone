866e3fe41df4bbd04ed0c10a678f241e83ae0600
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-12 15:13:53 +0500

    delete post

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { createPost } from '../controllers/postController.js';
+import { createPost, deletePost, commentOnPost } from '../controllers/postController.js';
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
 // router.post("/like/:id", protectRoute, likeUnlikePost);
-// router.post("/comment/:id", protectRoute, commentOnPost);
-router.delete("/", protectRoute, deletePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
 
 export default router;
