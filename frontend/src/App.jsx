626be8b98e6b1e527275e4e31b526da435273ef1
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-13 16:33:04 +0500

    ui design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,25 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/signup/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
+import SideBar from "./components/common/SideBar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
 
   return (
     <>
       <div className='flex max-w-6xl mx-auto'>
+        <SideBar />
         <Routes>
           <Route path="/" element={<HomePage />} />  
-          <Route path="/" element={<LoginPage />} />  
-          <Route path="/" element={<SignUpPage />} />  
+          <Route path="/login" element={<LoginPage />} />  
+          <Route path="/signup" element={<SignUpPage />} />  
+          <Route path="/notifications" element={<NotificationPage />} />  
+          <Route path="/profile/:username" element={<ProfilePage />} />  
         </Routes> 
+        <RightPanel />
       </div>
     </>
   )
