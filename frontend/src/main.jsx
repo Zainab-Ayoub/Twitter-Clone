f406ccc0bf131b663512dfb1c0dc5be0d41b2a93
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-09-14 16:34:12 +0500

    queryClient

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,13 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
  
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      reflectOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
