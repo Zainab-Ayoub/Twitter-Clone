91a2ac00f501514abdf7cb875dc89288e09fcac3
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-08 18:17:18 +0500

    commentPost

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -75,7 +75,7 @@ const Post = ({ post }) => {
 		},
 	});
 
-	const { mutate: commentPost, isPending: isCommenting } = useMutation({
+	const { mutate: comlikementPost, isPending: isCommenting } = useMutation({
 		mutationFn: async () => {
 			try {
 				const res = await fetch(`/api/posts/comment/${post._id}`, {
