46b4b79cf6f5732b8aa6ed706f06f89c8269267b
Zainab-Ayoub <zainabzafar365@gmail.com>
2024-10-09 20:00:16 +0500

    useQuery

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,36 +1,27 @@
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
-
+import { useQuery, useMutation } from "@tanstack/react-query";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
+	const { data: notifications, isLoading } = useQuery({
+		queryKey: ["notifications"],
+		queryFn: async () => {
+			try {
+			  const res = await fetch("/api/notifications");
+			  const data = await res.json();
+			  if (!res.ok) {
+				throw new Error(data.error || "Something went wrong");
+			  }	
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
+			  return data;
+			} catch (error) {
+			  throw new Error(error);	
+			}
+		},
+	});
 
 	return (
 		<>
